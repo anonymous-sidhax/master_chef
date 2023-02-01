e88467a16a21604561e5bc2e24fec6407528d66b
siddeshshewde <siddesh.shewde@gmail.com>
2023-02-01 17:07:50 -0500

    added import for collection

--- master_chef/settings.py
+++ master_chef/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import django_heroku
 import os
+from collections import Mapping
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
